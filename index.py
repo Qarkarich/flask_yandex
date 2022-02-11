a0cfafea6a37c5c1743ceca54c257f8dd562c3ae
Fedor Blinov <fedyoiblinov@gmail.com>
2022-02-11 18:24:11 +0300

    img promotion

--- index.py
+++ index.py
@@ -27,37 +27,35 @@ def index():
                 </html>'''
 
 
-@app.route('/promotion')
+@app.route('/promotion_image')
 def promote_page():
-    return '''<!DOCTYPE html>
-                <html>
-                <head>
-                    <title>Реклама!</title>
-                </head>
-                <body>
-                    <ul style="list-style: none">
-                                <li>Человечество вырастает из детства.</li>
-                                <li>Человечеству мала одна планета.</li>
-                                <li>Мы сделаем обитаемыми безжизненные пока планеты.</li>
-                                <li>И начнем с Марса!</li>
-                                <li>Присоединяйся!</li>
-                    </ul>
-                </body>
-                </html>'''
-
-
-@app.route('/image_mars')
-def show_mars():
     return f'''<!DOCTYPE html>
                 <html>
                 <head>
-                    <title>Привет, Марс!</title>
+                    <title>Колонизация</title>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                 </head>
                 <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="{url_for('static', filename='images/mars.png')}" alt="Красная 
                     планета!" width="500"><br>
                     <span>Вот она какая, красная планета</span>
+                    <ul style="list-style: none">
+                                <li class="alert alert-primary" role="alert">Человечество 
+                                вырастает из детства.</li>
+                                <li class="alert alert-secondary" role="alert">Человечеству мала 
+                                одна планета.</li>
+                                <li class="alert alert-success" role="alert">Мы сделаем обитаемыми 
+                                безжизненные пока планеты.</li>
+                                <li class="alert alert-warning" role="alert">И начнем с Марса!</li>
+                                <li class="alert alert-primary" role="alert">Присоединяйся!</li>
+                    </ul>
                 </body>
                 </html>'''
 
