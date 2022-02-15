44329644923fbbccb6cf29aabe0db03ee0b1d69a
Fedor Blinov <fedyoiblinov@gmail.com>
2022-02-15 18:58:12 +0300

    prof display

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for
 
-
 app = Flask(__name__)
 
 
@@ -8,12 +7,19 @@ app = Flask(__name__)
 @app.route('/index/<title>')
 def index(title='Миссия колонизации Марса!'):
     style_path = url_for('static', filename='css/style.css')
-    return render_template('base.html', title=title, style_path=style_path)
+    return render_template('base.html', title=title)
 
 
 @app.route('/training/<prof>')
 def prof_map(prof):
-    return render_template()
+    if 'инженер' in prof.lower() or 'строитель' in prof.lower():
+        heading = 'Инженерные тренажеры'
+        filename = url_for('static', filename='img/2.png')
+    else:
+        heading = 'Научные симуляторы'
+        filename = url_for('static', filename='img/1.png')
+
+    return render_template('prof.html', prof_heading=heading, filename=filename)
 
 
 if __name__ == '__main__':
