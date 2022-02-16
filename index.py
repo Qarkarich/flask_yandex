0169cd4bd561f7f164d2d475e791f2e5096e8fff
Fedor Blinov <fedyoiblinov@gmail.com>
2022-02-16 20:14:48 +0300

    prof list

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index/<title>')
 def index(title='Миссия колонизации Марса!'):
-    style_path = url_for('static', filename='css/style.css')
     return render_template('base.html', title=title)
 
 
@@ -20,6 +19,15 @@ def prof_map(prof):
         filename = url_for('static', filename='img/1.png')
 
     return render_template('prof.html', prof_heading=heading, filename=filename)
+
+
+@app.route('/list_prof/<list>')
+def prof_list(list):
+    professions = ['геолог', 'строитель', 'экзобиолог', 'инженер-биолог', 'экзобиолог',
+                   'инженер ракетных двигателей', 'астрофизик']
+    if list in ['ul', 'ol']:
+        return render_template('prof_list.html', list_type=list, prof_list=professions)
+    return render_template('error.html')
 
 
 if __name__ == '__main__':
